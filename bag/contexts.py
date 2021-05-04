6989697a8cd6c5d215c344c1fe544531ba161306
mcranter <mcranter@gmail.com>
2021-05-04 21:50:15 +0000

    updated shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-
 def bag_contents(request):
 
     bag_items = []
@@ -12,10 +11,12 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
 
     for item_id, quantity in bag.items():
-        product - get_object_or_404(Product, pk=item_id)
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
         bag_items.append({
             'item_id': item_id,
-            'quantity': quanity,
+            'quantity': quantity,
             'product': product,
         })
 
@@ -25,9 +26,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
