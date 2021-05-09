cd484bb864dfcaf657a82bb5a593bc0f36feeb13
mcranter <mcranter@gmail.com>
2021-05-09 11:37:39 +0000

    toasts added, programme and associated files removed to reflect new DB structure

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,11 +10,13 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
+
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
             product_count += item_data
+       
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
--- programmes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.all_programmes, name='programmes'),
-    path('<programme_id>', views.programme_detail, name='programme_detail')
-]
