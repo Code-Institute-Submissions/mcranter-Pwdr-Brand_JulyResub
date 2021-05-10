9c37819bf5cce770e3634bec584a4d0605c0a667
mcranter <mcranter@gmail.com>
2021-05-10 16:44:59 +0000

    added basic checkout functionality

---
+++ checkout/server.py
@@ -0,0 +1,77 @@
+# This example sets up an endpoint using the Flask framework. NON CI
+# from stripe.com documentation.
+import os
+import stripe
+import json
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+stripe.api_key = 'sk_test_51Igp6uAkbdkgFtr8YdD643EUTFkUi3Ymro0V0CTkVd6TpsOvIvw3OBebKXf1tHVschXc8FAf2IIZ5gaHysywLHoO000FwtKEPc'
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+  data = json.loads(request.data)
+
+  try:
+    # See https://stripe.com/docs/api/checkout/sessions/create
+    # for additional parameters to pass.
+    # {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+    # the actual Session ID is returned in the query parameter when your customer
+    # is redirected to the success page.
+    checkout_session = stripe.checkout.Session.create(
+        success_url='https://example.com/success.html?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url='https://example.com/canceled.html',
+        payment_method_types=['card'],
+        mode='subscription',
+        line_items=[{
+            'price': data['priceId'],
+            # For metered billing, do not pass quantity
+            'quantity': 1
+        }],
+        )
+        return jsonify({'sessionId': checkout_session['id']})
+    except Exception as e:
+        return jsonify({'error': {'message': str(e)}}), 400
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook_received():
+    webhook_secret = {{'STRIPE_WEBHOOK_SECRET'}}
+    request_data = json.loads(request.data)
+
+    if webhook_secret:
+        # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
+        signature = request.headers.get('stripe-signature')
+        try:
+            event = stripe.Webhook.construct_event(
+                payload=request.data, sig_header=signature, secret=webhook_secret)
+            data = event['data']
+        except Exception as e:
+            return e
+        # Get the type of webhook event sent - used to check the status of PaymentIntents.
+        event_type = event['type']
+    else:
+        data = request_data['data']
+        event_type = request_data['type']
+    data_object = data['object']
+
+    if event_type == 'checkout.session.completed':
+    # Payment is successful and the subscription is created.
+    # You should provision the subscription and save the customer ID to your database.
+      print(data)
+    elif event_type == 'invoice.paid':
+    # Continue to provision the subscription as payments continue to be made.
+    # Store the status in your database and check when a user accesses your service.
+    # This approach helps you avoid hitting rate limits.
+      print(data)
+    elif event_type == 'invoice.payment_failed':
+    # The payment failed or the customer does not have a valid payment method.
+    # The subscription becomes past_due. Notify your customer and send them to the
+    # customer portal to update their payment information.
+      print(data)
+    else:
+      print('Unhandled event type {}'.format(event_type))
+
+    return jsonify({'status': 'success'})
--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,41 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
+from bag.contexts import bag_contents
 from .forms import OrderForm
+
+import stripe
 
 
 # Create your views here.
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your bag is empty right now")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'HOLD UP - your stripe public key is missing.\
+            Did you forget to set it in your environment, dummy?')
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Igp6uAkbdkgFtr8wO7oDdioKa5VwcgRhgUm5bQo47wjY7aF4LDfs27cwvbrjRRIZAHJjyAU7smE6f8LYmN74fkD00yB5CzGPp',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
