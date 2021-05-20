56fd6c2290fba53fa0e4886b97040f887709aadc
mcranter <mcranter@gmail.com>
2021-05-20 09:02:12 +0000

    product admin: product form

--- profiles/views.py
+++ profiles/views.py
@@ -15,11 +15,9 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-        else:
-            messages.success(request, 'Update failed. Please check the form is valid.')
-    else:
-            form = UserProfileForm(instance=profile)
-        orders = profile.orders.all()
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
@@ -35,7 +33,7 @@ def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
-        f'This is a confirmation for order number: {order_number}. '
+        f'This is a past confirmation for order number {order_number}. '
         'A confirmation email was sent on the order date.'
     ))
 
