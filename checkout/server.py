d277b0311729bf9e51546987277c45fe7342fd31
mcranter <mcranter@gmail.com>
2021-05-22 19:19:50 +0000

    formatting

--- checkout/server.py
+++ checkout/server.py
@@ -58,20 +58,20 @@ def webhook_received():
     data_object = data['object']
 
     if event_type == 'checkout.session.completed':
-    # Payment is successful and the subscription is created.
-    # You should provision the subscription and save the customer ID to your database.
-      print(data)
+        # Payment is successful and the subscription is created.
+        # You should provision the subscription and save the customer ID to your database.
+            print(data)
     elif event_type == 'invoice.paid':
-    # Continue to provision the subscription as payments continue to be made.
-    # Store the status in your database and check when a user accesses your service.
-    # This approach helps you avoid hitting rate limits.
-      print(data)
+        # Continue to provision the subscription as payments continue to be made.
+        # Store the status in your database and check when a user accesses your service.
+        # This approach helps you avoid hitting rate limits.
+        print(data)
     elif event_type == 'invoice.payment_failed':
-    # The payment failed or the customer does not have a valid payment method.
-    # The subscription becomes past_due. Notify your customer and send them to the
-    # customer portal to update their payment information.
-      print(data)
+        # The payment failed or the customer does not have a valid payment method.
+        # The subscription becomes past_due. Notify your customer and send them to the
+        # customer portal to update their payment information.
+        print(data)
     else:
-      print('Unhandled event type {}'.format(event_type))
+        print('Unhandled event type {}'.format(event_type))
 
     return jsonify({'status': 'success'})
