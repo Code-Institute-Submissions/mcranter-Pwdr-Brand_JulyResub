3eecf6bc46f96e5097dbc26df3c99be3112de9f2
mcranter <mcranter@gmail.com>
2021-05-26 20:17:56 +0000

    404 testing

--- pwdr/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
-
-def handler404(request, exception):
-    response = render(request, "templates/404.html")
-    response.status_code = 404
-    return response
