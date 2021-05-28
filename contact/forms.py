ccf4bfcbc2a4068cd1cc98ba2bd458bcd1ab0f32
mcranter <mcranter@gmail.com>
2021-05-28 20:58:43 +0000

    fixed issue with footer not filling screen on movile

--- contact/forms.py
+++ contact/forms.py
@@ -5,4 +5,4 @@ class ContactForm(forms.Form):
     first_name = forms.CharField(max_length=40)
     last_name = forms.CharField(max_length=40)
     email_address = forms.EmailField(max_length=120)
-    message = forms.CharField(widget=forms.Textarea, max_length=2000)
+    message = forms.CharField(widget=forms.Textarea, max_length=1000)
