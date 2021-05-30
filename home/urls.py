97bc85d76ccc76d1b1368853fba29608bd989e9c
mcranter <mcranter@gmail.com>
2021-05-30 15:44:01 +0000

    final css corrections on index, contact,  etc page

--- home/urls.py
+++ home/urls.py
@@ -18,5 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('faqs', views.faqs, name='faqs'),
+    path('about', views.about, name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    """A view to return thw index page """
+    """A view to return the index page """
     return render(request, 'home/index.html')
+
+
+def faqs(request):
+    """A view to return the faqs page """
+    return render(request, 'home/faqs.html')
+
+
+def about(request):
+    """A view to return the faqs page """
+    return render(request, 'home/about.html')
