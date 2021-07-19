22bfdb993b4834595b0b20958c27f99dc0593757
mcranter <mcranter@gmail.com>
2021-07-19 12:36:04 +0000

    reviews templated amended, delete & edit views added

--- reviews/views.py
+++ reviews/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .models import Review
-from .forms import ReviewForm
 from products.models import Product
 from profiles.models import UserProfile
+
+from .models import Review
+from .forms import ReviewForm
 
 
 # Create your views here.
 @login_required
 def add_review(request, product_id):
     """
-    Add a review to a product
+    Add a product review
     """
     product = get_object_or_404(Product, pk=product_id)
     user = get_object_or_404(UserProfile, user=request.user)
@@ -42,3 +43,42 @@ def add_review(request, product_id):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_review(request, review_id):
+    """
+    Edit a product review
+
+    """
+    review = get_object_or_404(Review, pk=review_id)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Review Edit Complete.')
+        else:
+            messages.error(request, 'Failed to edit review. \
+                    Please check the form details are valid and try again.')
+    else:
+        form = ReviewForm(instance=review)
+
+    template = "reviews/edit_review.html"
+    context = {
+        "form": form,
+        "review": review,
+        "product": review.product,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_review(request, review_id):
+    """
+    Delete product review
+    """
+    review = get_object_or_404(Review, pk=review_id)
+    review.delete()
+    messages.success(request, 'Review deleted!')
+    return redirect(reverse('product_detail', args=(review.product.id,)))
